cbd40a4a43db25297da8e4b61a4c18811a4cba68
Liberland Software Foundation <info@libersoft.org>
2024-08-01 20:58:19 +0200

    Add splash screen, user info page

--- src/web/script.js
+++ src/web/script.js
@@ -5,56 +5,64 @@ document.addEventListener('DOMContentLoaded', async function () {
  Telegram.WebApp.ready();
  window.Telegram.WebApp.expand();
  await auth(Telegram.WebApp.initData);
- await getMenuPage();
 });
+
+async function auth(data) {
+ const res = await f.getAPI('login', { data: data });
+ console.log(res);
+ let html = '';
+ if (res && res.hasOwnProperty('error')) {
+  switch (res.error) {
+   case 0:
+    localStorage.setItem('data', JSON.stringify(res.data));
+    await getMenuPage();
+    break;
+   default:
+    html = f.translate(await f.getFileContent('html/login-error.html'), { '{ERROR}': res.message ? res.message : 'Unknown' });
+    f.qs('#splash .loader').outerHTML = html;
+  }
+ } else {
+  html = await f.getFileContent('html/login-unrecognized.html');
+  f.qs('#splash .loader').outerHTML = html;
+ }
+}
 
 async function getMenuPage() {
  const html = await f.getFileContent('html/menu.html');
- f.qs('#game').innerHTML = html;
+ document.body.innerHTML = html;
  await getScore();
 }
 
 async function getScore() {
  const res = await f.getAPI('score');
- f.qs('#game .score').innerHTML = res && res.hasOwnProperty('error') && res.error == 0 ? res.data.score : 'ERROR: ' + res.message;
+ f.qs('#menu .score').innerHTML = res && res.hasOwnProperty('error') && res.error == 0 ? res.data.score : 'ERROR: ' + res.message;
 }
 
 async function getStartGamePage() {
  const html = await f.getFileContent('html/game.html');
- f.qs('#game').innerHTML = html;
+ document.body.innerHTML = html;
  await dealCards(5, 4);
 }
 
 async function getHighScorePage() {
  const html = await f.getFileContent('html/highscore.html');
- f.qs('#game').innerHTML = html;
+ document.body.innerHTML = html;
 }
 
-async function auth(data) {
- const res = await f.getAPI('login', { data: data });
- let html = '';
- if (res && res.hasOwnProperty('error')) {
-  switch (res.error) {
-   case 0:
-    html = f.translate(await f.getFileContent('html/login-ok.html'), {
-     '{QUERY_ID}': res.data.query_id,
-     '{TIME}': new Date(res.data.auth_date * 1000).toLocaleString(),
-     '{USER_ID}': res.data.user.id,
-     '{USER_NAME}': res.data.user.username,
-     '{FIRST_NAME}': res.data.user.first_name,
-     '{LAST_NAME}': res.data.user.last_name,
-     '{LANGUAGE}': res.data.user.language_code,
-     '{PREMIUM}': res.data.user.is_premium ? 'Yes' : 'No',
-     '{PM}': res.data.user.allows_write_to_pm ? 'Yes' : 'No'
-    });
-    break;
-   default:
-    html = f.translate(await f.getFileContent('html/login-error.html'), { '{ERROR}': res.message ? res.message : 'Unknown' });
-  }
- } else {
-  html = await f.getFileContent('html/login-unrecognized.html');
- }
- f.qs('#userinfo').innerHTML = html;
+async function getUserInfoPage() {
+ const data = JSON.parse(localStorage.getItem('data'));
+ const html = f.translate(await f.getFileContent('html/userinfo.html'), {
+  '{QUERY_ID}': data.query_id,
+  '{TIME}': new Date(data.auth_date * 1000).toLocaleString(),
+  '{USER_ID}': data.user.id,
+  '{USER_NAME}': data.user.username,
+  '{FIRST_NAME}': data.user.first_name,
+  '{LAST_NAME}': data.user.last_name,
+  '{LANGUAGE}': data.user.language_code,
+  '{PREMIUM}': data.user.is_premium ? 'Yes' : 'No',
+  '{PM}': data.user.allows_write_to_pm ? 'Yes' : 'No'
+ });
+ document.body.innerHTML = html;
 }
 
 async function dealCards(x, y) {
@@ -62,13 +70,11 @@ async function dealCards(x, y) {
  const cardTemp = await f.getFileContent('html/card.html');
  let html = '';
  for (let i = 1; i <= y; i++) {
-  //html += '<div class="row">';
   for (let j = 1; j <= x; j++) {
    html += f.translate(cardTemp, {
     '{ID}': x * (i - 1) + j
    });
   }
-  //html += '</div>';
  }
  f.qs('#cards').innerHTML = html;
 }
